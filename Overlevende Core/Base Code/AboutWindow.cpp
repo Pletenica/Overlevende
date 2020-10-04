aa07d6351eaab2debf02b1227bbb709d3b163def
Yuye <47743107+Rugiacreed@users.noreply.github.com>
2020-10-04 16:44:07 +0200

    readme actualized

--- Overlevende Core/Base Code/AboutWindow.cpp
+++ Overlevende Core/Base Code/AboutWindow.cpp
@@ -18,8 +18,23 @@ AboutWindow::~AboutWindow()
 void AboutWindow::CreateAboutWindow() {
 	ImGui::Begin("About", NULL);
 
-	///////////////// MODES ///////////////////////
-	ImGui::Text("Choose your favourite interface mode.");
+	///////////////// Readme ///////////////////////
+	ImGui::Text("This is an engine done by Roger Perez Romera and Josep Sanchez Arbona.");
+	ImGui::Text("");
+	ImGui::Text("This project began from a subject in the grade Videogamaes Design and Development in CITM UPC Terrassa, we are two in-progress developers");
+	ImGui::Text("that want to give the best experience through our engine: Overlende.");
+	ImGui::Text("");
+	ImGui::Text("Copyright 2020 Josep Sanchez Arbona and Roger Perez Romera.");
+	ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files");
+	ImGui::Text("(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,");
+	ImGui::Text("publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do");
+	ImGui::Text("so, subject to the following conditions:");
+	ImGui::Text("");
+	ImGui::Text("THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF ");
+	ImGui::Text("MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE");
+	ImGui::Text("FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION");
+	ImGui::Text("WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
+
 	/*
 	PER A FER ELS BOTONS EN IMATGES (En un futur)
 	if (ImGui::ImageButton(iconprovingtex, ImVec2(100, 40))) {
@@ -27,9 +42,6 @@ void AboutWindow::CreateAboutWindow() {
 	}
 	*/
 
-
-	///////////////// COLORS ///////////////////////
-	ImGui::Text("Choose your favourite interface color:");
 	
 
 	if (ImGui::Button("Close Window", ImVec2(100, 25))) {
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # OVERLEVENDE
 This is an engine done by Roger Pérez Romera and Josep Sànchez Arbona.
+This project began from a subject in the grade Videogamaes Design and Development in CITM UPC Terrassa, we are two in-progress developers that want to give the best experience
+through our engine: Overlende. 
+
+Copyright 2020 Josep Sànchez Arbona and Roger Pérez Romera.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
